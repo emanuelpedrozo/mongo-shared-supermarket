98897bafa22ea6f6fb4258f9383f62aaa564f7cd
emanuelpedrozo <75386495+emanuelpedrozo@users.noreply.github.com>
2024-06-04 17:04:28 -0300

    Delete setup_sharding.js

--- setup_sharding.js
+++
@@ -1,7 +0,0 @@
-sh.addShard("mongo1:27017")
-sh.addShard("mongo2:27017")
-sh.addShard("mongo3:27017")
-
-sh.enableSharding("supermercado")
-
-sh.shardCollection("supermercado.produtos", { "filial_id": 1 })
